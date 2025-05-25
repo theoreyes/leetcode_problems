b3772a1d6831bcef47f1019a01c816d079304797
Ubuntu <theodore.anthony.reyes@gmail.com>
2025-05-25 08:55:17 +0000

    Optimized Longest Unique Substring solution

--- med/Longest_Substring_Without_Repeating_Characters.py
+++ med/Longest_Substring_Without_Repeating_Characters.py
@@ -1,19 +1,25 @@
-# Inefficient implementation of this problem, will update with a O(n)
-# solution
+# Theodore Reyes
+#
+# Explanation: Uses a single pass through the string, where
+# the last-seen position of each character is kept track
+# of in a hash table. When repeats are found, the start of the 
+# current substring is moved 1 past the repeat's last-seen index,
+# and the length updated accordingly. The greatest length value
+# reached is returned as output.
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         length = 0
         record = 0
-        seen = set()
-        for i in range(len(s)):
-            substr = s[i:]
-            for ch in substr:
-                if ch in seen:
-                    seen.clear()
-                    length = 0
-                    break
-                seen.add(ch)
-                length += 1
-                if length > record : record = length
+        start = 0
+        seen = {}
+        for i, ch in enumerate(s):
+            if ch in seen and seen[ch] >= start:
+                lastpos = seen[ch]
+                start = lastpos + 1
+                seen[ch] = i
+                length = i - lastpos - 1
+            length += 1
+            if length > record : record = length
+            seen[ch] = i
         return record
