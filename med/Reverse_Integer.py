b11f52ad9f178c29aaebf47edb02621173782a5d
Theodore Anthony Reyes <theodore.anthony.reyes@gmail.com>
2025-05-27 17:01:22 -0700

    Updated Reverse_Integer.py

--- med/Reverse_Integer.py
+++ med/Reverse_Integer.py
@@ -4,28 +4,28 @@
 # to result integer, shifting the input down a factor of 10 and
 # the result number up a factor of 10 after each iteration.
 #
-# Note: Going to try and optimize this to avoid the sign check,
-# just wanted to get the first shot uploaded for now
+# Note: Re-wrote partially for clarity as well as to comply with
+# the 32-bit constraint of operands. My overflow check consists
+# of the semantic question "Can we handle doing the following
+# operation given current value of result and tmp?"
+# In practice, this looks like calculating the largest value
+# of result that we can fit in 32-bits and then checking if
+# the actual value is greater than this. This is simply to
+# avoid overflowing during the multiply and add step found
+# on the last line of the while loop :)
 
 class Solution:
     def reverse(self, x: int) -> int:
         INT_MAX = 2147483647
-        INT_MIN = -2147483648
         result = 0
-        inpNum = x
         tmp = 0
-        sign = 0
-        if (inpNum < 0):
-            sign = 1
-            inpNum *= -1
-        while inpNum > 0:
-            tmp = inpNum % 10
-            inpNum //= 10
-            result += tmp
-            result *= 10
-        result //= 10
+        sign = x < 0
+        if sign: x *= -1
+        while x > 0:
+            tmp = x % 10
+            x //= 10
+            if result > ((INT_MAX) - tmp) // 10: # avoids overflowing 32-bits
+                return 0
+            result = result * 10 + tmp
         if (sign == 1): result *= -1
-        if result > INT_MAX or result < INT_MIN:
-            return 0
-        else:
-            return result
+        return result
